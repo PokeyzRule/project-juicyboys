e552110e21cd8697edc52b190ca1f691582dfd5e
Ammar <ammart500@gmail.com>
2021-05-31 16:06:38 -0400

    JUIC-1 Server setup and DB connection

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// DB Connection
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// Server setup
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
