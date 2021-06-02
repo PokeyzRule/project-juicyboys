56f7179758f4259469baadedfcbce4fdf22b6848
skeshavaa <s.keshavaa@gmail.com>
2021-06-01 22:34:52 -0400

    JUIC-3 Initialized frontend with redux and project structure

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeStyles from './Home.module.scss'
+
+function Home() {
+    return (
+        <div className={HomeStyles.testingContainer}>
+            This is Home
+        </div>
+    )
+}
+
+export default Home
