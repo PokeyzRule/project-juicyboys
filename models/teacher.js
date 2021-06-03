1ce3e8113fb544ae419212b0f57754ae75e3ee02
Vignesh <70033044+VigneshNk@users.noreply.github.com>
2021-06-02 23:11:10 -0400

    JUIC-7 completed teacher schema

--- models/teacher.js
+++ models/teacher.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const teacherSchema = new Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    teacherID: {
+        type:String,
+        required: true
+    },
+    currentCourses: {
+        type: [String],
+        required: true
+    }
+})
+
+module.exports = Teacher = mongoose.model('teacher', teacherSchema)
