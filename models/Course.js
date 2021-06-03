64e62bdce7565ce70306b6f3e78f0206d2940847
Gordon Xiong <axionggordon@yahoo.ca>
2021-06-03 16:19:52 -0400

    JUIC-12 created and completed course schema

---
+++ models/Course.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CourseSchema = new Schema({
+    name: {
+        type: String,
+        required: True,
+    },
+    teacher: {
+        type: String,
+        required: True,
+    },
+    description: {
+        type: String,
+        required: True,
+    },
+    students: {
+        type: [String],
+        default: [],
+    },
+    posts: {
+        type: [Object],
+        default: [],
+    },
+    color: {
+        type: String,
+        required: True,
+    },
+    courseID: {
+        type: Schema.Types.ObjectId,
+        required: True,
+        unique: True,
+    },
+})
+
+module.exports = Course = mongoose.model('course', CourseSchema)
