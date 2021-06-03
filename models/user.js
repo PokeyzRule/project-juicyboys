a0f4c5b368fc7e96cf149741d2f8e5e4786a22b5
Vignesh <70033044+VigneshNk@users.noreply.github.com>
2021-06-02 23:32:24 -0400

    JUIC-7 created and completed user schema

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    currentCourses: {
+        type: [String],
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    ID: {
+      type: String,
+      required: true
+    }
+})
+
+module.exports = User = mongoose.model('user', userSchema)
