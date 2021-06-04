89923fd481aff0783ab1a2fb52f6bc5cb26572c3
skeshavaa <s.keshavaa@gmail.com>
2021-06-03 23:03:48 -0400

    JUIC-21 implemented signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/home'
 import './App.css'
-import Login from './pages/login'
+import SignUp from './pages/signup'
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={SignUp} />
         <Route path="/login" exact component={Login} />
       </BrowserRouter>
     </div>
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,78 +1,35 @@
 import React, { useState } from 'react'
-import LoginStyles from './Login.module.scss'
-import LoginImage from '../../assets/Login.png'
-import UserSelect from '../../components/userSelect'
-import student from '../../assets/student.png'
-import teacher from '../../assets/teacher.png'
-import entrepreneur from '../../assets/entrepreneur.png'
+import LoginStyles from '../signup/Login.module.scss'
 import { Link } from 'react-router-dom'
+import LoginImage from '../../assets/Login.png'
 
 function Login() {
-    const [step, setStep] = useState(0)
-    const [user, setUser] = useState("");
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-
-    const Form = () => {
-
-        if (step == 0){
-            return(
-                <div>
-                    <h2>Which one are you?</h2>
-                    <div className={LoginStyles.selectContainer}>
-                        <UserSelect userType="Student" image={student} active={user == "Student"} handleInput={handleInput}/>
-                        <UserSelect userType="Teacher" image={teacher} active={user == "Teacher"} handleInput={handleInput}/>
-                        <UserSelect userType="Entrepreneur" image={entrepreneur} active={user == "Entrepreneur"} handleInput={handleInput}/>
-                    </div>
-                    <div className={LoginStyles.next}>
-                        <button onClick={(e) => {setStep(1)}} style={{backgroundColor: user == "" ? "#9A9AAC" : "#6C63FF"}}>Next</button>
-                    </div>
-                </div>
-            )
-        }else{
-            return(
-                <div className={LoginStyles.credentialsContainer}>
-                    <p className={LoginStyles.label}>Name</p>
-                    <input className={LoginStyles.input} onChange={(e) => {setName(e.target.value)}}/>
-                    <p className={LoginStyles.label}>Email</p>
-                    <input className={LoginStyles.input} onChange={(e) => {setEmail(e.target.value)}}/>
-                    <p className={LoginStyles.label}>Password</p>
-                    <input type="password" className={LoginStyles.input} onChange={(e) => {setPassword(e.target.value)}} />
-
-                    <div className={LoginStyles.next}>
-                        <button onClick={(e) => {setStep(0)}} style={{backgroundColor: "#9A9AAC" }}>Back</button>
-                    </div>
-
-                    <div className={LoginStyles.next}>
-                        <button onClick={(e) => {console.log("registering!")}} style={{backgroundColor:"#6C63FF"}}>Register</button>
-                    </div>
-                </div>
-            )
-        }
-
-    }
-
-
-    function handleInput(userType){
-        setUser(userType)
-    }
-
+    const [email, setEmail] = useState("")
 
     return (
         <div className={LoginStyles.wrapper}>
             <div className={LoginStyles.container}>
-                <img src={LoginImage} />
+                
                 <div className={LoginStyles.formContainer}>
                     <div>
-                        <h1 className={LoginStyles.header}>Join Us Today!</h1>
-                        <p className={LoginStyles.subheader}>Become part of a growing community of students, teachers, and entrepreneurs</p>
-                        <Form />
+                        <h1 className={LoginStyles.header}>Login!</h1>
+                        <p className={LoginStyles.subheader}>Welcome Back! Check out the new classes/startups on the homepage that you may be interested in!</p>
+
+                        <div className={LoginStyles.credentialsContainer}>
+                            <p className={LoginStyles.label}>Email</p>
+                            <input className={LoginStyles.input} onChange={(e) => {setEmail(e.target.value)}}/>
+                            <p className={LoginStyles.label}>Password</p>
+                            <input type="password" className={LoginStyles.input} onChange={(e) => {setPassword(e.target.value)}} />
+                            <div className={LoginStyles.next}>
+                                <button onClick={(e) => {console.log("registering!")}} style={{backgroundColor:"#6C63FF"}}>Login</button>
+                            </div>
+                        </div>
                     </div>
-                    <p className={LoginStyles.alternate}>Already have an account? <Link className={LoginStyles.sign}>Login here!</Link></p>
+                    <p className={LoginStyles.alternate}>Don't have an account? <Link to="/signup" className={LoginStyles.sign}>Sign Up Here!</Link></p>
                 </div>
+                <img src={LoginImage} />
             </div>
-            
         </div>
     )
 }
