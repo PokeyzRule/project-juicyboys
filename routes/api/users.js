39fc28caec1c7c1579d15d84156f0c86f5638642
Vignesh <vigneshnanthak@gmail.com>
2021-06-07 19:46:28 -0400

    JUIC-8 added specific object creation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,6 @@ const Teacher = require('../../models/teacher');
 const Entrepreneur = require('../../models/entrepreneur');
 
 
-
 //Registering user
 router.post('/register', (req, res) => {
 
@@ -17,65 +16,6 @@ router.post('/register', (req, res) => {
     if (!name || !email || !password || !type) {
         return res.status(400).json({status: "failure", msg: "Please enter all fields"})
     }
-
-    if (Student.findOne({email}) ||  Teacher.findOne({email}) || Entrepreneur.findOne({email})) {
-        return res.status(400).json({status: "failure", msg: "User already exists!"});
-    }
-
-    if(type == "Student"){
-        const newUser = new Student({
-            name,
-            email,
-            currentCourses,
-            password
-        })
-
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser.save()
-                .then(user => {
-
-                    jwt.sign(
-                        { id: user.id },
-                        'africanImpactProject',
-                        { expiresIn: 3600 },
-                        (err, token) => {
-                            if (err) throw err;
-                            res.status(200).json(
-                                {
-                                    token,
-                                    user: {
-                                        id: user.id,
-                                        name: user.name,
-                                        email: user.email,
-                                        currentCourses: []
-                                    }
-                                }
-                            )
-                        }
-                    )
-                })
-            })
-        })
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
 
     User.findOne({email}).then(user => {
         if (user){
@@ -101,6 +41,43 @@ router.post('/register', (req, res) => {
                         { expiresIn: 3600 },
                         (err, token) => {
                             if (err) throw err;
+
+                            // Create and upload student object
+                            if(type == "Student"){
+                                const typedUser = new Student({
+                                    name: user.name,
+                                    email: user.email,
+                                    currentCourses: [],
+                                    password: hash,
+                                    studentID: user.id
+                                })
+                                typedUser.save();
+                            }
+
+                            // Create and upload teacher object
+                            if(type == "Teacher"){
+                                const typedUser = new Teacher({
+                                    name: user.name,
+                                    email: user.email,
+                                    password: hash,
+                                    currentCourses: [],
+                                    teacherID: user.id
+                                })
+                                typedUser.save();
+                            }
+
+                            // Create and upload entrepreneur object
+                            if(type == "Entrepreneur"){
+                                const typedUser = new Entrepreneur({
+                                    name: user.name,
+                                    email: user.email,
+                                    password: hash,
+                                    associatedStartups: [],
+                                    associatedStartups: user.id
+                                })
+                                typedUser.save();
+                            }
+
                             res.status(200).json(
                                 {
                                     token,
@@ -117,12 +94,6 @@ router.post('/register', (req, res) => {
             })
         })
     })
-
-
-
-
 })
-
-
 
 module.exports = router
