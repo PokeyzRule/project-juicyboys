975ff988af5a6674d6a434f3eb0b7785047bfa4a
Keshavaa Shaiskandan <s.keshavaa@gmail.com>
2021-06-08 19:19:52 -0400

    Feature/JUIC-20 (#18)
    * JUIC-20 basic landing created

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import HomeStyles from './Home.module.scss'
+import Navbar from '../../components/Navbar'
+import styles from './Home.module.scss'
 
 function Home() {
     return (
-        <div className={HomeStyles.testingContainer}>
-            This is Home
+        <div className={styles.testingContainer}>
+            <Navbar />
+            <div className={styles.overlay}>
+                <h1 className={styles.welcome}>Welcome back <span className={styles.highlight}>Keshavaa!</span></h1>
+            </div>
+            <div className={styles.classes}>
+                <h3>New Classes</h3>
+                <p>Join new and upcoming classes now!</p>
+            </div>
+            <div className={styles.divider}></div>
         </div>
     )
 }
