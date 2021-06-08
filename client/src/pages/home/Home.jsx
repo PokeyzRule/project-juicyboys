b6291d7a207b39d0ffe5fb991f38c8c7ed4ab014
skeshavaa <s.keshavaa@gmail.com>
2021-06-08 00:00:55 -0400

    basic landing page implemented

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import HomeStyles from './Home.module.scss'
+import Navbar from '../../components/Navbar'
+import styles from './Home.module.scss'
 
 function Home() {
     return (
-        <div className={HomeStyles.testingContainer}>
-            This is Home
+        <div className={styles.testingContainer}>
+            <Navbar />
+            <div className={styles.overlay}>
+                <h1 className={styles.welcome}>Welcome back <span className={styles.highlight}>Keshavaa!</span></h1>
+            </div>
         </div>
     )
 }
