453ed3573f1323c482e5f631ecd643bcda67bd28
skeshavaa <s.keshavaa@gmail.com>
2021-06-08 15:07:03 -0400

    JUIC-20 basic landing created

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -9,6 +9,11 @@ function Home() {
             <div className={styles.overlay}>
                 <h1 className={styles.welcome}>Welcome back <span className={styles.highlight}>Keshavaa!</span></h1>
             </div>
+            <div className={styles.classes}>
+                <h3>New Classes</h3>
+                <p>Join new and upcoming classes now!</p>
+            </div>
+            <div className={styles.divider}></div>
         </div>
     )
 }
