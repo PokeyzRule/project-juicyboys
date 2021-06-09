fcf7457d75622307d22ce0b1406df58a9f192ca7
Jackie Chen <jackiechenzq@gmail.com>
2021-06-08 20:05:29 -0400

    JUIC-17 set up overall website

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/home'
 import './App.css'
 import SignUp from './pages/signup'
 import Login from './pages/login';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={SignUp} />
         <Route path="/login" exact component={Login} />
+        <Route path="/profile" exact component={Profile} />
       </BrowserRouter>
     </div>
   );
