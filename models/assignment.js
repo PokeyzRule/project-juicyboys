7f058995b80f254c9468b552a31e60751dcaf9c1
Vignesh <70033044+VigneshNk@users.noreply.github.com>
2021-06-23 01:42:16 -0400

    JUIC-36 completed assignment schema

--- models/assignment.js
+++ models/assignment.js
@@ -1,1 +1,39 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const AssignmentSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  uploads: {
+    type: [String],
+    default: []
+  },
+  submissions: {
+    type: [String],
+    default: []
+  },
+  comments: {
+    type: [Object],
+    default: []
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  assignedDate: {
+    type: Date,
+    required: true
+  },
+  assignmentID: {
+    type: Schema.Types.ObjectId,
+    auto: true
+  }
+})
+
+module.exports = mongoose.model('assignment', AssignmentSchema)
