c92bc5107ecd6dc51f285483f0c42a81ab492dfc
Ammar <ammart500@gmail.com>
2021-06-23 00:34:08 -0400

    JUIC-39 Add timestamps for posts/comments

--- models/comment.js
+++ models/comment.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const CommentSchema = new Schema({
-  userID: {
-    type: String,
-    required: true,
+const CommentSchema = new Schema(
+  {
+    userID: {
+      type: String,
+      required: true,
+    },
+    postID: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    }
   },
-  postID: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  }
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('comment', CommentSchema)
