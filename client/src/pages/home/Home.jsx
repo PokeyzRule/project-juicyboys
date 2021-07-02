6d8c190f522fd960d962e93acecd919ce758c2a2
skeshavaa <s.keshavaa@gmail.com>
2021-07-01 23:45:48 -0400

    JUIC-33 implemented search bar on landing page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -7,7 +7,9 @@ import { useHistory } from 'react-router-dom'
 import courseAPI from '../../api/courseAPI'
 
 function Home() {
+
     const { state } = useContext(AuthContext)
+    const [ allCourses, setAllCourses ] = useState([])
     const [ courses, setCourses ] = useState([]);
     const [ loading, setLoading ] = useState(true)
     const history = useHistory()
@@ -19,9 +21,19 @@ function Home() {
     useEffect(() => {
         courseAPI.getAllCourses().then((response) => {
             setCourses(response.data.courses)
+            setAllCourses(response.data.courses)
             setLoading(false)
         })
     }, [])
+
+    const handleSearch = (e) => {
+        if (e.target.value != ""){
+            const filteredCourses = courses.filter((course) => course.name.toLowerCase().includes(e.target.value.toLowerCase()) || course.teacher.toLowerCase().includes(e.target.value.toLowerCase()));
+            setCourses(filteredCourses)
+        }else{
+            setCourses(allCourses)
+        }
+    }
 
     return (
         <div className={styles.testingContainer}>
@@ -29,11 +41,17 @@ function Home() {
             <div className={styles.overlay}>
                 <h1 className={styles.welcome}>Welcome back <span className={styles.highlight}>Keshavaa!</span></h1>
             </div>
-            <div className={styles.classes}>
-                <h3>New Classes</h3>
-                <p>Join new and upcoming classes now!</p>
+            <div className={styles.header}>
+                <div className={styles.classes}>
+                    <h3>New Classes</h3>
+                    <p>Join new and upcoming classes now!</p>
+                </div>
+                <div className={styles.searchContainer}>
+                    <input placeholder={"Search by name or teacher!"} className={styles.search} onChange={(e) => handleSearch(e)}/>
+                </div>
             </div>
             <div className={styles.divider}></div>
+            
             <div className={styles.courseContainer}>
                 {loading ? <h1>Loading</h1> : 
                 courses.map((course) => {
