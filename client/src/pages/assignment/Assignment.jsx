729484536723dc72be147ed9e0ba955ead78fff3
Jackie Chen <jackiechenzq@gmail.com>
2021-07-02 15:58:42 -0400

    JUICY-26 added assignment form for teachers > * initial commit > * routes for api > * frontend is mostly done

--- client/src/pages/assignment/Assignment.jsx
+++ client/src/pages/assignment/Assignment.jsx
@@ -3,71 +3,109 @@ import assignmentStyles from './Assignment.module.scss'
 import Navbar from '../../components/Navbar'
 import axios from 'axios'
 import { AuthContext } from '../../App'
+import courseAPI from '../../api/courseAPI'
+import teacherAPI from '../../api/teacherAPI'
 import Course from '../../components/Course'
-
 
 function Assignment() {
 
     const { state, dispatch } = useContext(AuthContext)
+    const [ title, setTitle] = useState("")
+    const [ description, setDescription] = useState("")
+    const [ courseID, setCourseID] = useState("")
+    
+    // const [ uploads, setUploads] = useState("")
+    // const [ submissions, setSubmissions] = useState("")
+    // const [ comments, setComments] = useState("")
+    const [ assignedDate, setAssignedDate] = useState("")
+    const [ assignmentID, setAssignmentID] = useState("")
+
+
+    const d = new Date()
+    const dueDate = d.getTime();
+    console.log(state)
     const [user, setUser] = useState()
     const [loading, setLoading] = useState(true)
 
-    console.log(state)
+    const [ newCourseName, setNewCourseName] = useState("")
+    const [ newCourseDescription, setNewCourseDescription] = useState("")
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/students/${state.user.id}`, {
-            headers: {
-                token: state.token
-            }
-        }).then((resp) => {
-            setUser(resp.data)
-            console.log(resp.data)
+            teacherAPI.getTeacherByID(state.user.id).then((response) => {
+            setUser(response.data)
             setLoading(false)
         })
-    }, [])
+    }, []) 
+    function newCourse(e){
+        let req = {
+            name: newCourseName,
+            teacher: state.user.id,  //NEED TO CHANGE THIS TO TEACHER
+            description: newCourseDescription,
+            color: "#ffffff"
+        }
+        courseAPI.insertCourse(req).then((response) => {
+            
+        })
+    }
+    function newAssignment(e) {
+        e.preventDefault();
 
+        const data = JSON.stringify({ title, description, courseID, dueDate, assignedDate, assignmentID })
+
+        
+    }
     return (
         <div>
             < Navbar />
-            <div className={assignmentStyles.infoBar}>
-                <div className={assignmentStyles.info}>
-                    <h1>Name</h1>
-                    <h2>{state.user.name}</h2>
-                </div>
-                <div className={assignmentStyles.info}>
-                    <h1>Email</h1>
-                    <h2>{state.user.email}</h2>
-                </div>
-                <div className={assignmentStyles.info}>
-                    <h1>Bio</h1>
-                    <h2>Lorem Ipsum</h2>
-                </div>
-            </div>
+
             <div className={assignmentStyles.wrapper}>
 
                 <div className={assignmentStyles.container}>
 
                     <div className={assignmentStyles.contentContainer}>
                         <div>
+                            <div className={assignmentStyles.credentialsContainer}>
+                                <h1 className={assignmentStyles.header}>Create Course</h1>
+                                <p className={assignmentStyles.label}>Course Name</p>
+                                <input className={assignmentStyles.input} onChange={(e) => {setNewCourseName(e.target.value)}}/>
+                                <p className={assignmentStyles.label}>Description</p>
+                                <input className={assignmentStyles.input} onChange={(e) => {setNewCourseDescription(e.target.value)}}/>
+                                <div className={assignmentStyles.next}>
+                                    <button onClick={(e) => newCourse(e)} style={{backgroundColor:"#6C63FF"}}>Create Assignment</button>
+                                </div>
 
+                            </div>
+                            <br/><br/>
                             <div>
-                                <h1 className={assignmentStyles.header}>User Info</h1>
-                                <p className={assignmentStyles.subheader}>Your Recent Courses</p>
-                                {loading ?
-                                    <h1>Loading</h1>
-                                    :
+                                <div className={assignmentStyles.credentialsContainer}>
+                                    <h1 className={assignmentStyles.header}>Create new assigment by selecting course</h1>
                                     <div className={assignmentStyles.courseContainer}>
-                                        {user.courses.map((course) => {
-                                            return (
-                                                <Course course={course} />
-                                            )
-                                        })}
+                                        {/* {loading ? <h1>Loading</h1> : 
+                                        // courses.map((course) => {
+                                        //     return(
+                                        //         <Course course={course}/>
+                                        //     )
+                                        // })
+                                        } */}
                                     </div>
-                                }
+                                    <p className={assignmentStyles.label}>Title</p>
+                                    <input className={assignmentStyles.input} onChange={(e) => {setTitle(e.target.value)}}/>
+                                    <p className={assignmentStyles.label}>Description</p>
+                                    <input className={assignmentStyles.input} onChange={(e) => {setDescription(e.target.value)}}/>
+                                    <p className={assignmentStyles.label}>CourseID</p>
+                                    <input className={assignmentStyles.input} onChange={(e) => {setAssignedDate(e.target.value)}}/>
+                                    <p className={assignmentStyles.label}>AssignmentID</p>
+                                    <input className={assignmentStyles.input} onChange={(e) => {setAssignmentID(e.target.value)}}/>
+                                    
+                                    <div className={assignmentStyles.next}>
+                                        <button onClick={(e) => newAssignment(e)} style={{backgroundColor:"#6C63FF"}}>Create Assignment</button>
+                                    </div>
+                                </div>
+                                
                                 <hr width="100%" size="2" color="#C4C4C4" />
                                 <br />
                             </div>
-                            <p className={assignmentStyles.subheader}>Don't forget your deadlines!</p>
+                            <p className={assignmentStyles.subheader}>Please Fill Out all Fields</p>
                         </div>
                     </div>
                 </div>
