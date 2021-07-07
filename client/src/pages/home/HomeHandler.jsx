33f0ed27b6f86e2a84da0db6f11532409c19293a
skeshavaa <s.keshavaa@gmail.com>
2021-07-07 13:26:03 -0400

    JUIC-65 refactored home page to route to appropriate home page based of user

---
+++ client/src/pages/home/HomeHandler.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Navbar from '../../components/Navbar'
+import styles from './Home.module.scss'
+import {AuthContext} from '../../App'
+import Course from '../../components/Course'
+import StudentHome from './StudentHome'
+import { useHistory } from 'react-router-dom'
+import TeacherHome from './TeacherHome'
+import EntrepreneurHome from './EntrepreneurHome'
+
+function HomeHandler() {
+
+    const { state } = useContext(AuthContext)
+    const history = useHistory()
+    
+    if (!state.isAuthenticated){
+        history.push("/login")
+    }
+
+    if (state.user.type == "student"){
+        return(
+            <StudentHome />
+        )
+    }else if (state.user.type == "teacher"){
+        return(
+            <TeacherHome />
+        )
+    }else{
+        return(
+            <EntrepreneurHome />
+        )
+    }
+}
+
+export default HomeHandler
+
