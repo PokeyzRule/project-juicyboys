7f2517bb7a46bae11cb70a7422f20d3309079b66
Ammar <ammart500@gmail.com>
2021-07-08 19:05:14 -0400

    JUIC-54 Added assignment route

--- client/src/components/CourseAssignment/CourseAssignment.jsx
+++ client/src/components/CourseAssignment/CourseAssignment.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './CourseAssignment.module.scss'
-
+import { Link } from 'react-router-dom'
 
 function CourseAssignment({ assignment }) {
 
@@ -15,17 +15,17 @@ function CourseAssignment({ assignment }) {
     }
 
     return (
-        <div className={styles.container}>
-            <div className={styles.background}>
-                <h1 className={styles.title}>{assignment.title}</h1>
-                <h2 className={styles.subtitle}>Due Date: {parseDate(assignment.dueDate)}</h2>
+        <Link to={`/course/${assignment.courseID}/assignments/${assignment.assignmentID}`} style={{textDecoration: 'none'}}>
+            <div className={styles.container} >
+                <div className={styles.background}>
+                    <h1 className={styles.title}>{assignment.title}</h1>
+                    <h2 className={styles.subtitle}>Due Date: {parseDate(assignment.dueDate)}</h2>
+                </div>
+                <div className={styles.info}>
+                    <p className={styles.desc}>{assignment.description}</p>
+                </div>
             </div>
-            <div className={styles.info}>
-                <p className={styles.desc}>{assignment.description}</p>
-                <p className={styles.details}>Click to view details</p>
-                {/* Make the whole box clickable link to more detailed assignment page */}
-            </div>
-        </div>
+        </Link>
     )
 }
 
