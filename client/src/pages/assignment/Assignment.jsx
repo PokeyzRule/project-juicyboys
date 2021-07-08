cc0a27e971a7dcf7c4c132049764226687f01aa2
Ammar <ammart500@gmail.com>
2021-07-08 18:39:05 -0400

    JUIC-54 Created Assignment Page

--- client/src/pages/assignment/Assignment.jsx
+++ client/src/pages/assignment/Assignment.jsx
@@ -1,114 +1,93 @@
-import React, { useState, useEffect, useContext } from 'react'
-import assignmentStyles from './Assignment.module.scss'
-import Navbar from '../../components/Navbar'
-import axios from 'axios'
-import { AuthContext } from '../../App'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
 import courseAPI from '../../api/courseAPI'
-import teacherAPI from '../../api/teacherAPI'
-import Course from '../../components/Course'
+import styles from './Assignment.module.scss'
+import Navbar from '../../components/Navbar'
+import { Button } from '@material-ui/core'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import GetAppIcon from '@material-ui/icons/GetApp'
+import fileDownload from 'js-file-download'
+import axios from 'axios'
 
-function Assignment() {
+const Assignment = () => {
 
-    const { state, dispatch } = useContext(AuthContext)
-    const [ title, setTitle] = useState("")
-    const [ description, setDescription] = useState("")
-    const [ courseID, setCourseID] = useState("")
-    
-    // const [ uploads, setUploads] = useState("")
-    // const [ submissions, setSubmissions] = useState("")
-    // const [ comments, setComments] = useState("")
-    const [ dueDate, setDueDate] = useState("")
-    const [ assignmentID, setAssignmentID] = useState("")
-
-
-    const d = new Date()
-    const assignedDate = d.getTime();
-    console.log(state)
-    const [user, setUser] = useState()
+    const { cid, aid } = useParams()
+    const [assignment, setAssignment] = useState()
+    const [course, setCourse] = useState()
     const [loading, setLoading] = useState(true)
 
-    const [ newCourseName, setNewCourseName] = useState("")
-    const [ newCourseDescription, setNewCourseDescription] = useState("")
+    useEffect(() => {
+        courseAPI.getCourseByID(cid).then((resp) => {
+            setCourse(resp.data.course)
+            resp.data.assignments.map((a) => {
+                if (a.assignmentID == aid) {
+                    setAssignment(a)
+                    setLoading(false)
+                    return
+                }
+            })
+        })
+    }, [])
 
-    // useEffect(() => {
-    //         teacherAPI.getTeacherByID(state.user.id).then((response) => {
-    //         setUser(response.data)
-    //         setLoading(false)
-    //     })
-    // }, []) 
-    function newCourse(e){
-        // let req = {
-        //     name: newCourseName,
-        //     teacher: state.user.id,  //NEED TO CHANGE THIS TO TEACHER
-        //     description: newCourseDescription,
-        //     color: "#ffffff"
-        // }
-        // courseAPI.insertCourse(req).then((response) => {
-            
-        // })
+    const UploadLink = ({link}) => {
+        return (
+            <Button
+                variant="contained"
+                color="primary"
+                className={styles.uploadLinks}
+                endIcon={<GetAppIcon />}
+                onClick={() => {handleDownload('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg', 'test-download.jpg')}}
+            >
+                {link}
+            </Button>
+        )
     }
-    function newAssignment(e) {
-        // e.preventDefault();
 
-        // const data = JSON.stringify({ title, description, courseID, dueDate, assignedDate, assignmentID })
-
-        
+    const handleDownload = (url, filename) => {
+        axios.get(url, {
+          responseType: 'blob',
+        })
+        .then((res) => {
+          fileDownload(res.data, filename)
+        })
     }
+
     return (
         <div>
-            < Navbar />
-
-            <div className={assignmentStyles.wrapper}>
-
-                <div className={assignmentStyles.container}>
-
-                    <div className={assignmentStyles.contentContainer}>
-                        <div>
-                            <h1 className={assignmentStyles.header}>Create Course</h1>
-                            <div className={assignmentStyles.credentialsContainer}>
-                                <p className={assignmentStyles.label}>Course Name</p>
-                                <input className={assignmentStyles.input} onChange={(e) => {setNewCourseName(e.target.value)}}/>
-                                <p className={assignmentStyles.label}>Description</p>
-                                <input className={assignmentStyles.input} onChange={(e) => {setNewCourseDescription(e.target.value)}}/>
-                                <div className={assignmentStyles.next}>
-                                    <button onClick={(e) => newCourse(e)} style={{backgroundColor:"#6C63FF"}}>Create Course</button>
-                            </div>
-
-                            </div>
-                            <br/><br/>
-                            <div>
-                                <h1 className={assignmentStyles.header}>Create New Assigment By Selecting Course</h1>
-                                <div className={assignmentStyles.credentialsContainer}>
-                                    <div className={assignmentStyles.courseContainer}>
-                                        {/* {loading ? <h1>Loading</h1> : 
-                                        // courses.map((course) => {
-                                        //     return(
-                                        //         <Course course={course}/>
-                                        //     )
-                                        // })
-                                        } */}
-                                    </div>
-                                    <p className={assignmentStyles.label}>Title</p>
-                                    <input className={assignmentStyles.input} onChange={(e) => {setTitle(e.target.value)}}/>
-                                    <p className={assignmentStyles.label}>Description</p>
-                                    <input className={assignmentStyles.input} onChange={(e) => {setDescription(e.target.value)}}/>
-                                    <p className={assignmentStyles.label}>Due Date</p>
-                                    <input className={assignmentStyles.input} onChange={(e) => {setDueDate(e.target.value)}}/>
-                                    
-                                    <div className={assignmentStyles.next}>
-                                        <button onClick={(e) => newAssignment(e)} style={{backgroundColor:"#6C63FF"}}>Create Assignment</button>
-                                    </div>
-                                </div>
-                                
-                                <hr width="100%" size="2" color="#C4C4C4" />
-                                <br />
-                            </div>
-                            <p className={assignmentStyles.subheader}>Please Fill Out all Fields</p>
-                        </div>
-                    </div>
+            <Navbar />
+            <div className={styles.infoBar}>
+                <div className={styles.info}>
+                    <h2>{loading ? 'Loading...' : `${course.name} --- ${course.teacher}` }</h2>
+                    <h1> {loading ? 'Loading...' : assignment.title}</h1>
+                    <h2>Created: {loading ? 'Loading...' : new Date(assignment.assignedDate).toLocaleString()}</h2>
+                    <h2>Due Date: {loading ? 'Loading...' : assignment.dueDate ? new Date(assignment.dueDate).toLocaleString() : 'N/A'}</h2>
                 </div>
-
             </div>
+            <div className={styles.description}>
+                <h2>Assignment Overview</h2>
+                <hr style={{ maxWidth: 260 }}/>
+                <h3>{loading ? 'Loading...' : assignment.description}</h3>
+            </div>
+            <div className={styles.description}>
+                <h2>Relevant Files</h2>
+                <hr style={{ maxWidth: 170 }}/>
+                <div className={styles.uploads}>
+                    {loading ? <h1> 'Loading...' </h1> : assignment.uploads.map((upload) => <UploadLink link={upload} />)}
+                </div>
+            </div>
+            { loading ? <h1> 'Loading...' </h1> : 
+            assignment.toSubmit ? 
+                <div className={styles.description}>
+                    <h2>Upload Submissions</h2>
+                    <hr style={{ maxWidth: 250 }}/>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        startIcon={<CloudUploadIcon />}
+                    >
+                        Upload Files
+                    </Button>
+                </div> : <div/> }
         </div>
     )
 }
