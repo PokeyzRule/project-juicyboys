adcca2c763843b46d3333cfac81000690060d459
Gordon Xiong <axionggordon@yahoo.ca>
2021-07-16 21:39:04 -0400

    JUIC-68 Add new owner feature done

--- client/src/api/entrepreneurAPI.js
+++ client/src/api/entrepreneurAPI.js
@@ -2,9 +2,11 @@ import axiosClient from "../utils/axiosClient";
 
 // GET
 export const getEntrepreneurByID = (id) => axiosClient.get(`/entrepreneurs/${id}`);
+export const getEntrepreneurByEmail = (email) => axiosClient.get(`/entrepreneurs/email/${email}`)
 
 const entrepreneurAPI = {
-    getEntrepreneurByID
+    getEntrepreneurByID,
+    getEntrepreneurByEmail,
 };
 
 export default entrepreneurAPI;
--- routes/api/entrepreneur.js
+++ routes/api/entrepreneur.js
@@ -5,8 +5,32 @@ const auth = require('../../middleware/auth')
 const Entrepreneur = require('../../models/entrepreneur')
 
 /**
+ * @route   GET /email/:email
+ * @desc    Fetch an entrepreneur's profile by email
+ * @access  Authenticated users
+ */
+router.get('/email/:email', auth, async (req, res) => {
+    console.log(req)
+    try {
+        email = req.params.email
+        entrepreneur = await Entrepreneur.findOne({ email: email })
+        return res.status(200).json({
+            user: entrepreneur,
+            status: 'Success',
+            message: 'Entrepreneur profile fetched successfully!'
+        })
+    } catch {
+        res.status(400).json({
+            status: 'Failure',
+            message: 'Unable to retreieve entrepreneur\'s profile via email'
+        })
+    }
+
+})
+
+/**
  * @route   GET /:id
- * @desc    Fetch a teacher's profile
+ * @desc    Fetch an entrepreneur's profile
  * @access  Authenticated users
  */
 router.get('/:id', auth, (req, res) => {
