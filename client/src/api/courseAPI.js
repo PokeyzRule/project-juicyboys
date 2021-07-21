8c00ecefa2f0a081bb91f159ecc6793e9a14f704
Ammar <ammart500@gmail.com>
2021-07-21 16:29:32 -0400

    JUIC-57 Added submissions tables for teachers

--- client/src/api/courseAPI.js
+++ client/src/api/courseAPI.js
@@ -4,6 +4,7 @@ import axiosClient from "../utils/axiosClient";
 export const insertCourse = (payload) => axiosClient.post(`/courses/create`, payload);
 export const enrollCourse = (payload) => axiosClient.post(`/courses/enroll`, payload);
 export const addAssignment = (payload) => axiosClient.post('/courses/createAssignment', payload)
+export const addAssignmentFiles = (payload) => axiosClient.post(`/courses/updateAssignment`, payload)
 export const addSubmission = (payload) => axiosClient.post('/courses/submitAssignment', payload)
 
 // GET
@@ -14,6 +15,7 @@ const courseAPI = {
     insertCourse,
     enrollCourse,
     addAssignment,
+    addAssignmentFiles,
     addSubmission,
     getAllCourses,
     getCourseByID,
--- client/src/pages/assignment/Assignment.jsx
+++ client/src/pages/assignment/Assignment.jsx
@@ -13,24 +13,33 @@ import axios from 'axios'
 import uploadAPI from '../../api/uploadAPI'
 import MuiAlert from '@material-ui/lab/Alert';
 import { Snackbar } from '@material-ui/core'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const Assignment = () => {
 
     const { cid, aid } = useParams()
     const { state } = useContext(AuthContext)
-    const uid = JSON.parse(state.user).id;
+    const user = JSON.parse(state.user);
     const [assignment, setAssignment] = useState()
     const [course, setCourse] = useState()
-    const [uploads, setUploads] = useState([])
+    const [submissions, setSubmissions] = useState([])
+    const [files, setFiles] = useState([])
     const [loading, setLoading] = useState(true)
     const [open, setOpen] = React.useState(false);
-
+    
     useEffect(() => {
         courseAPI.getCourseByID(cid).then((resp) => {
             setCourse(resp.data.course)
             resp.data.assignments.map((a) => {
                 if (a.assignmentID == aid) {
                     setAssignment(a)
+                    setFiles(a.uploads)
                     setLoading(false)
                     return
                 }
@@ -63,20 +72,37 @@ const Assignment = () => {
 
     const handleFileUpload = (e) => {
         const file = e.target.files[0]
-        
         uploadAPI.addFile(file).then((data) => {
-            setUploads(uploads => [...uploads, data])
+            const newFiles = files.concat(data.location)
+            setFiles(newFiles)
+            const payload = {
+                uploads: Array.from(newFiles),
+                assignmentID: aid
+            }
+            courseAPI.addAssignmentFiles(payload)
         })
     }
 
-    const handleDelete = (name) => {
-        setUploads(uploads => uploads.filter(upload => upload.key !== name))
+    const handleFileDelete = (name) => {
+        setFiles(files => files.filter(file => file.key !== name))
     }
 
-    const handleSubmit = () => {
+    const handleSubmissionUpload = (e) => {
+        const file = e.target.files[0]
+        
+        uploadAPI.addFile(file).then((data) => {
+            setSubmissions(submissions => [...submissions, data])
+        })
+    }
+
+    const handleSubmissionDelete = (name) => {
+        setSubmissions(submissions => submissions.filter(submission => submission.key !== name))
+    }
+
+    const handleSubmissionSubmit = () => {
         const payload = {
-            uploads: Array.from(uploads.map((upload) => upload.location)),
-            studentID: uid,
+            uploads: Array.from(submissions.map((submission) => submission.location)),
+            studentID: user.id,
             assignmentID: aid
         }
         
@@ -110,22 +136,41 @@ const Assignment = () => {
                 <h2>Relevant Files</h2>
                 <hr style={{ maxWidth: 170 }}/>
                 <div className={styles.uploads}>
-                    {loading ? <h1> 'Loading...' </h1> : assignment.uploads.map((upload) => <UploadLink link={upload} />)}
+                    {loading ? <h1> 'Loading...' </h1> : files.map((file) => <UploadLink link={file} />)}
                 </div>
+                {/* <div style={{ marginBottom: '10px' }}>
+                        {files.map((file) => <Chip key={file.key} className={styles.file} label={file.key} onDelete={() => handleFileDelete(file.key)} />)}
+                </div> */}
+                { user.type == "teacher" ? 
+                    <div>
+                        <input
+                            hidden
+                            id="contained-button-file"
+                            type="file"
+                            onChange={handleFileUpload}
+                        />
+                        <label htmlFor="contained-button-file">
+                            <Button variant="contained" color="secondary" component="span" className={styles.uploadButton} startIcon={<CloudUploadIcon />}>
+                                Upload Files
+                            </Button>
+                        </label>
+                    </div>
+                    : <div />
+                }
             </div>
             { loading ? <h1> 'Loading...' </h1> : 
-            assignment.toSubmit ? 
+            assignment.toSubmit && user.type == "student" ? 
                 <div className={styles.description}>
                     <h2>Upload Submissions</h2>
                     <hr style={{ maxWidth: 250 }}/>
                     <div style={{ marginBottom: '10px' }}>
-                        {uploads.map((upload) => <Chip key={upload.key} className={styles.file} label={upload.key} onDelete={() => handleDelete(upload.key)} />)}
+                        {submissions.map((submission) => <Chip key={submission.key} className={styles.file} label={submission.key} onDelete={() => handleSubmissionDelete(submission.key)} />)}
                     </div>
                     <input
                         hidden
                         id="contained-button-file"
                         type="file"
-                        onChange={handleFileUpload}
+                        onChange={handleSubmissionUpload}
                     />
                     <label htmlFor="contained-button-file">
                         <Button variant="contained" color="secondary" component="span" className={styles.uploadButton} startIcon={<CloudUploadIcon />}>
@@ -136,17 +181,46 @@ const Assignment = () => {
                         variant="contained"
                         color="primary"
                         endIcon={<Send/>}
-                        onClick={(e) => handleSubmit(e)}
+                        onClick={(e) => handleSubmissionSubmit(e)}
                     >
                         Submit Assignment!
                     </Button>
                 </div>
-                : <div/> }
-                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-                    <MuiAlert elevation={6} variant="filled" onClose={handleClose} severity="success">
-                        Successfully submitted assignment!
-                    </MuiAlert>
-                </Snackbar>
+            : <div/> }
+            { loading ? <h1> 'Loading...' </h1> : 
+            assignment.toSubmit && user.type == "teacher" ? 
+                <div className={styles.description}>
+                    <h2>Student Submissions</h2>
+                    <hr style={{ maxWidth: 250 }}/>
+                    <TableContainer className={styles.table} component={Paper}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell align="right">Name</TableCell>
+                                <TableCell align="right">Uploads</TableCell>
+                                <TableCell align="right">Upload Date</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {assignment.submissions.map((submission) => (
+                                <TableRow key={submission.studentName}>
+                                    <TableCell>{submission.studentID}</TableCell>
+                                    <TableCell align="right">{submission.studentName}</TableCell>
+                                    <TableCell align="right">{submission.uploads.map((upload) => (<h3><a href={upload}>{upload}</a></h3>))}</TableCell>
+                                    <TableCell align="right">{new Date(submission.date).toLocaleString()}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </div>
+            : <div/> }
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <MuiAlert elevation={6} variant="filled" onClose={handleClose} severity="success">
+                    Successfully submitted assignment!
+                </MuiAlert>
+            </Snackbar>
         </div>
     )
 }
