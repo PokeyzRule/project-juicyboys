fc40ca4c7a4b8ff832db0647e762b8f1b2a0986c
skeshavaa <s.keshavaa@gmail.com>
2021-07-26 23:00:03 -0400

    JUIC-95 refactored profile code to account for different types of users

---
+++ client/src/pages/profile/TeacherProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TeacherProfile() {
+    return (
+        <div>
+            This is a teacher profile
+        </div>
+    )
+}
+
+export default TeacherProfile
