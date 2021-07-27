5bae48ffccb4907cb155c712533ed0fcdbd383e3
Vignesh <70033044+VigneshNk@users.noreply.github.com>
2021-07-27 01:23:24 -0400

    JUIC-87 removed extra model import

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
-const company = require('../../models/company')
 
 // Models
 const Company = require('../../models/company')
